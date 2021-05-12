5be7a0300eeaefd0fe00bc920b16c155f6ad5878
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-05-12 12:13:18 +0200

    project initialization and addition of recipe data

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# REACT-JS-Recette-gateau-chocolat
+# REACT-JS- Recette gateau au chocolat
+
 Pour les gourmands, une page 'recette de gâteau au chocolat' réalisée avec React JS
---
+++ src/data/recipe.js
@@ -0,0 +1,49 @@
+export default {
+  title: 'Gâteau au chocolat',
+  thumbnail: 'https://images.pexels.com/photos/53483/strawberries-crepe-dessert-sweet-53483.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+  author: 'Laurie Olait',
+  difficulty: 'Facile',
+  ingredients: [
+    {
+      id: 1,
+      quantity: 250,
+      unit: 'g',
+      name: 'de beurre',
+    },
+    {
+      id: 2,
+      quantity: 200,
+      unit: 'g',
+      name: 'de sucre',
+    },
+    {
+      id: 3,
+      quantity: 4,
+      unit: '',
+      name: 'oeufs eniers',
+    },
+    {
+      id: 4,
+      quantity: 250,
+      unit: 'g',
+      name: 'chocolat noir',
+    },
+    {
+      id: 5,
+      quantity: 70,
+      unit: 'g',
+      name: 'de farine',
+    },
+  ],
+  instructions: [
+    'Préchauffer le four à 180°C.',
+    'Faire fondre le chocolat et le beurre au bain marie.',
+    'Dans un saladier, battre les œufs avec le sucre jusqu\'à ce que le mélange mousse et blanchisse.',
+    'Rajouter le beurre fondu tout en mélangeant',
+    'Incorporer le mélange chocolat/beurre puis la farine.',
+    'Une fois la pâte homogène, la verser dans un moule beurré et fariné de 24/25 cm de diamètre.',
+    'Faire cuire 25 minutes environ.',
+    'Il faut ensuite attendre que le gâteau soit à température ambiante pour servir.',
+    'Bon appétit !',
+  ],
+};
