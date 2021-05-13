dc1cdd5b653b32bc88ee13d2173b02c1de517431
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-05-13 11:30:14 +0200

    Dynamization of the steps component + added list key

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -21,7 +21,7 @@ const App = () => (
       thumbnail={recipe.thumbnail}
     />
     <Ingredients />
-    <Steps />
+    <Steps steps={recipe.instructions} />
   </div>
 );
 
--- src/components/Steps/index.js
+++ src/components/Steps/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './steps.scss';
 
-const Steps = () => (
-  <section className="steps">
-    <ol className="steps__list">
-      <li className="steps__list__item">Préchauffer le four à 180°C.</li>
-      <li className="steps__list__item">Faire fondre le chocolat et le beurre au bain marie.</li>
-      <li className="steps__list__item">Dans un saladier, battre les œufs avec le sucre jusqu\'à ce que le mélange mousse et blanchisse.</li>
-      <li className="steps__list__item">Rajouter le beurre fondu tout en mélangeant.</li>
-      <li className="steps__list__item">Incorporer le mélange chocolat/beurre puis la farine.</li>
-      <li className="steps__list__item">Une fois la pâte homogène, la verser dans un moule beurré et fariné de 24/25 cm de diamètre.</li>
-    </ol>
-  </section>
+const Steps = ({ steps }) => (
+  <>
+    <section className="steps">
+      <ol className="steps__list">
+        {steps.map((step) => (
+          <li className="steps__list__item" key={step}>
+            {step}
+          </li>
+        ))}
+      </ol>
+    </section>
+  </>
 );
+
+Steps.propTypes = {
+  // array of strings
+  steps: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Steps;
